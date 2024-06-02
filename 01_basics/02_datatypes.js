f76e30e7f517820dec142d5d3b6e29116044d4a4
nikunjaa <136500216+nikunjaa@users.noreply.github.com>
2024-06-02 08:11:32 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";   //treat all JS code as newer version
+
+// alert ("hello") can be used in console. but here we are using nodejs and not browser
+
+//code readability is important for professional use. extensions can also be used. 
+
+let name = "Hitesh"
+let age = 18
+let isLoggedIn = false
+
+//number => 2^13
+//bigint
+//string => ""
+// boolean => true or false
+// null =>standalone value     variables can be defined as null to depict the *empty* space in the variable. not undefined but empty
+// undefined =>  
+// symbol => unique
+
+
+//object      (null is an object)
+
+console.log(typeof "hitesh");
+console.log(typeof null);  //object type
+console.log(typeof undefined); //undefined
