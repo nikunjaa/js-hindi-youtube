f9681a4eb906769b8dd4d6337eaadf98ff818ddf
nikunjaa <136500216+nikunjaa@users.noreply.github.com>
2024-06-03 15:01:14 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,28 @@
-let score = 33
+// let score = 33
+// let score = "33"
+// let score = "33abc"
+let score = true  
+// let score = null  
+
+
 
 console.log(typeof score);
 console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber)); 
+console.log(valueInNumber);     //  o/p  for 33abc= NaN, op for null = 0
+
+let isLoggedIn= ""
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true, 0=> false
+// ""  => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 js practice repo 
 
 Number and number is different for the datatype
+
+NaN -> Not a number
+null is an object
